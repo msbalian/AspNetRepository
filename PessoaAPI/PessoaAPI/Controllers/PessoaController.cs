f5d322bd6540740992a6ff2780b702c820daa015
unknown <marciobalian@gmail.com>
2021-07-08 15:30:42 -0300

    Inicio do Projeto PessoaAPI

---
+++ PessoaAPI/PessoaAPI/Controllers/PessoaController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PessoaAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PessoaController : ControllerBase
+    {
+        
+        private readonly ILogger<PessoaController> _logger;
+
+        public PessoaController(ILogger<PessoaController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("soma/{num1}/{num2}")]
+        public IActionResult Soma(int num1, int num2)
+        {
+            int soma = num1 + num2;
+            return Ok(soma.ToString());
+        }
+    }
+}
