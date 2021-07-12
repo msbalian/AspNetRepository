a11014be2358ebf8c1349026a626ad9022a72e40
unknown <marciobalian@gmail.com>
2021-07-12 18:49:02 -0300

    API com os verbos principais v-01

--- PessoaAPI/PessoaAPI/Services/Implementations/PessoaServiceImplementation.cs
+++ PessoaAPI/PessoaAPI/Services/Implementations/PessoaServiceImplementation.cs
@@ -20,16 +20,57 @@ namespace PessoaAPI.Services.Implementations
 
         public Pessoa Create(Pessoa pessoa)
         {
+
+            try
+            {
+                _context.Add(pessoa);
+                _context.SaveChanges();
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
             return pessoa;
         }
 
 
         public void Delete(Pessoa pessoa)
         {
+
+            try
+            {
+                _context.Remove(pessoa);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
         }
 
         public void DeleteById(long id)
         {
+            var pessoaBuscada = _context.Pessoas.Single(p => p.Id.Equals(id));
+
+            if (pessoaBuscada != null)
+            {
+
+                try
+                {
+                    _context.Pessoas.Remove(pessoaBuscada);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+
+            }
+
         }
 
         public List<Pessoa> FindAll()
@@ -47,6 +88,29 @@ namespace PessoaAPI.Services.Implementations
 
         public Pessoa Update(Pessoa pessoa)
         {
+            if (_context.Pessoas.Any(p => p.Id.Equals(pessoa.Id)))
+            {
+                var result = _context.Pessoas.SingleOrDefault(p => p.Id.Equals(pessoa.Id));
+                if (result != null)
+                {
+                    try
+                    {
+                        _context.Entry(result).CurrentValues.SetValues(pessoa);
+                        _context.SaveChanges();
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+
+                }
+
+            }
+            else
+            {
+                return new Pessoa();
+            }
+            
             return pessoa;
         }
         
